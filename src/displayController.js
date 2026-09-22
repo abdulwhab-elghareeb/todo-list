f4acf801fcaa6c1650a65669eae014e532635bbf
abdulwhab-elghareeb <abdulwhabelghareeb16@gmail.com>
2026-09-22 14:35:41 +0300

    Add functionality to projects counter

--- src/displayController.js
+++ src/displayController.js
@@ -7,13 +7,24 @@ import { createTask } from "./createTask.js"
 
 export const displayController = (() =>{
     const projectsList = document.querySelector("ul")
+    const projectsCounter = document.querySelector("#projects-counter")
     
     const addingProjectsForm = document.querySelector("#add-project-dialog > form")
     const projectTitleInput = document.querySelector("#project-title")
     const projectDescriptionTextarea = document.querySelector("#project-description")
 
+
+    function updateProjectsCounter(){ // updating the color of the counter depending on the amount of projects
+        projectsCounter.textContent = `${projectsContainer.getProjectsArray().length} / ${projectsContainer.getMaxLength()}` // current number of projects / max number of projects
+        
+        projectsContainer.getProjectsArray().length <= projectsContainer.getMaxLength() / 2? projectsCounter.classList = "low" : projectsCounter.classList = "mid"
+        if(projectsContainer.getProjectsArray().length == projectsContainer.getMaxLength()) projectsCounter.classList = "max"
+    }
+
     // Adding Projects
     addingProjectsForm.addEventListener("submit", () =>{
+        if (projectsContainer.getProjectsArray().length >= projectsContainer.getMaxLength()) return // if the projectsContainer's max limit is reached
+
         const project = createProject(projectTitleInput.value, projectDescriptionTextarea.value)
         projectsContainer.addProject(project)
         
@@ -41,7 +52,6 @@ export const displayController = (() =>{
         projectsList.appendChild(projectListItem)
 
         addingProjectsForm.reset()
+        updateProjectsCounter()
     })
-
-
 })()
